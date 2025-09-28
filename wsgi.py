e2ad63306026f70ca1562d7334e46fb7f8306883
HSSamuel <smkmayomisamuel@gmail.com>
2025-09-28 06:43:08 +0100

    update

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,3 @@
-# wsgi.py
-
 from backend.app import create_app
 
-# Unpack the tuple from create_app and assign the app object to 'application'
-application, _ = create_app()
+application = create_app()  # Create the app instance
